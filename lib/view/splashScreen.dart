1cf35fb91c7c6126deb7cdec22f215fa857fce5f
ralia <ali.roshan.nia.1379@gmail.com>
2025-05-31 19:25:51 +0330

    [IMPLEMENT] [add bouncing-dot-loader in splash screen]

--- lib/view/splashScreen.dart
+++ lib/view/splashScreen.dart
@@ -1,6 +1,7 @@
-import 'package:ejarika_app/main.dart';
 import 'package:ejarika_app/models/city.dart';
 import 'package:ejarika_app/services/ad_service.dart';
+import 'package:ejarika_app/utils/colors.dart';
+import 'package:ejarika_app/widgets/bouncing-dot-loader.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -36,29 +37,32 @@ class _SplashScreenState extends State<SplashScreen> {
     }
 
     await Future.delayed(const Duration(seconds: 2));
-    Navigator.pushReplacement(
-      context,
-      MaterialPageRoute(builder: (context) => const MainNavigationWrapper()),
-    );
+    // Navigator.pushReplacement(
+    //   context,
+    //   MaterialPageRoute(builder: (context) => const MainNavigationWrapper()),
+    // );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const FlutterLogo(size: 100),
-            const SizedBox(height: 20),
-            const CircularProgressIndicator(),
-            const SizedBox(height: 20),
-            const Text(
-              'در حال بارگذاری...',
-              style: TextStyle(fontFamily: 'YekanBakh', fontSize: 16),
+      backgroundColor: AppColors.primary,
+      body: Stack(
+        children: [
+          Center(
+            child: Image.asset(
+              'assets/images/logo.png',
+              width: 200,
             ),
-          ],
-        ),
+          ),
+
+          Positioned(
+            bottom: 50,
+            left: 0,
+            right: 0,
+            child: Center(child: BouncingDotsLoader()),
+          ),
+        ],
       ),
     );
   }
---
+++ lib/widgets/bouncing-dot-loader.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+import 'package:ejarika_app/utils/colors.dart';
+
+class BouncingDotsLoader extends StatefulWidget {
+  const BouncingDotsLoader({super.key});
+
+  @override
+  State<BouncingDotsLoader> createState() => _BouncingDotsLoaderState();
+}
+
+class _BouncingDotsLoaderState extends State<BouncingDotsLoader>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late List<Animation<double>> _dotAnimations;
+
+  final int numberOfDots = 3;
+  final double bounceHeight = 10;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = AnimationController(
+      duration: const Duration(milliseconds: 1),
+      vsync: this,
+    )..repeat();
+
+    _dotAnimations = List.generate(numberOfDots, (index) {
+      final start = (index * 0.3) % 1.0;
+      final end = (start + 0.3) % 1.0;
+      return TweenSequence([
+        TweenSequenceItem(
+          tween: Tween<double>(begin: 0, end: -bounceHeight),
+          weight: 50,
+        ),
+        TweenSequenceItem(
+          tween: Tween<double>(begin: -bounceHeight, end: 0),
+          weight: 50,
+        ),
+      ]).animate(
+        CurvedAnimation(
+          parent: _controller,
+          curve: Interval(
+            start,
+            end > start ? end : end + 1.0,
+            curve: Curves.easeInOut,
+          ),
+        ),
+      );
+    });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  Widget _buildDot(Animation<double> animation) {
+    return AnimatedBuilder(
+      animation: animation,
+      builder: (context, child) {
+        return Transform.translate(
+          offset: Offset(0, animation.value),
+          child: child,
+        );
+      },
+      child: Container(
+        width: 12,
+        height: 12,
+        margin: const EdgeInsets.symmetric(horizontal: 5),
+        decoration: const BoxDecoration(
+          color: AppColors.white,
+          shape: BoxShape.circle,
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: _dotAnimations.map((animation) => _buildDot(animation)).toList(),
+    );
+  }
+}
